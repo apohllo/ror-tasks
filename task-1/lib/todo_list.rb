a1ecda7e8108784cd5b3d1e16c609f88e01570bc
marziolek <ziolkenz@interia.pl>
2013-03-12 21:44:19 +0100

    First couple test working

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,31 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    if items == nil 
+      raise IllegalArgument
+    else
+      @items = items
+    end
   end
+      
+    def empty?
+      @items.empty?
+    end
+
+    def size
+      @items.size
+    end
+    
+    def <<(other_object)
+      @items << other_object
+    end 
+    
+    def last
+      @items.last
+    end
+
+    def first
+      @items.first
+    end 
+
 end
