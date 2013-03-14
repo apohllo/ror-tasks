8792687b6f1bb4c62cf97eff03b27beabb82f751
lukasz <lukasz@lukasz-linux.(none)>
2013-03-14 04:20:07 +0100

    task-1 - 1

--- task-1/spec/todo_list.rb
+++ task-1/spec/todo_list.rb
@@ -55,4 +55,57 @@ describe TodoList do
       list.completed?(0).should be_true
     end
   end
+
+  context "with many items" do
+    subject(:list)      { TodoList.new(items) }
+    let(:items)         { [1, 2, 3, 5, 4] }
+    
+    it "return completed items" do
+      list.complete(0)
+      list.complete(1)
+      list.get_completed.should == [1, 2]
+    end
+      
+    it "return uncompleted items" do
+      list.complete(0)
+      list.complete(1)
+      list.get_uncompleted.should == [3, 5, 4]
+    end
+
+    it "remove item" do
+      list.delete_at(1)
+      list.get_all_items.should == [1, 3, 5, 4]
+    end
+    
+    it "remove completed items" do
+      list.complete(0)
+      list.complete(1)
+      list.delete_completed.should == [3, 5, 4]
+    end
+
+    it "reverting order of two items" do
+      list.reverse(0, 1).should == [2, 1, 3, 5, 4]
+    end
+    
+    it "toggling the state of an item" do
+      list.toggle(1)
+      list.completed?(1).should be_true
+    end
+
+    it "setting the state of the item to uncompleted" do
+      list.complete(1)
+      list.uncomplete(1)
+      list.completed?(1).should be_false
+    end
+
+    it "changing the description of an item" do
+      list.set_description(1, 9).should == [1, 9, 3, 5, 4]
+    end
+
+    it "sorting items by name" do
+      list.sort.should == [1, 2, 3, 4, 5]
+    end
+
+    it "conversion of the list to text"
+  end
 end
