d315b10f33952550fb1e92e315847f25340fda64
Aleksander Pohl <apohllo@o2.pl>
2013-04-13 16:06:57 +0200

    Definitions of other helper methods
    Definitions for all helper methods and classes are written making the test
    report failures instead of errors.

--- task-3/test/acceptance/test_helper.rb
+++ task-3/test/acceptance/test_helper.rb
@@ -3,6 +3,19 @@ class Account
   end
 end
 
+class ExchangeRate
+  def initialize(source_currency,target_currency,value)
+  end
+end
+
+class CurrencyExchanger
+  def initialize(source_account,target_account,rate)
+  end
+
+  def exchange(amount=nil)
+  end
+end
+
 
 module CurrencyExchangeHelper
   def set_balance(accounts)
@@ -10,5 +23,28 @@ module CurrencyExchangeHelper
     accounts.each do |currency,value|
       @accounts << Account.new(currency,value)
     end
+  end
+
+  def set_exchange_rate(rates)
+    @rates ||= []
+    rates.each do |(source,target),value|
+      @rates << ExchangeRate.new(source,target,value)
+    end
+  end
+
+  def exchange_money(source,target,amount=nil)
+    exchanger = CurrencyExchanger.new(find_account(source),find_account(target),
+                                      find_rate(source,target))
+    exchanger.exchange(amount)
+  end
+
+  def get_balance(currency)
+    find_account(currency)
+  end
+
+  def find_account(currency)
+  end
+
+  def find_rate(source,target)
   end
 end
