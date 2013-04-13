f56bfcabd1e6dbccd7f88f20d5f2e350ca182b16
Aleksander Pohl <apohllo@o2.pl>
2013-04-13 20:25:52 +0200

    More specs for the money exchanger
    Specs for scenarios where the amount of available money is lower than the amount
    imposed by the limit.

--- task-3/test/spec/currency_exchanger.rb
+++ task-3/test/spec/currency_exchanger.rb
@@ -19,7 +19,7 @@ describe CurrencyExchanger do
     let(:source_final_amount)   { Money("0") }
     let(:target_final_amount)   { Money("415") }
 
-    it "should exchange all money" do
+    it "should exchange all money from the source account" do
       mock(source_account).withdraw(source_initial_amount)
       mock(target_account).deposit(target_final_amount)
       mock(calculator).compute_target_amount(source_initial_amount,rate) { target_final_amount }
@@ -40,6 +40,19 @@ describe CurrencyExchanger do
 
       exchanger.exchange(:source => limit)
     end
+
+    context "with the limit exceeding the money on the source account" do
+      let(:source_initial_amount) { Money("10") }
+      let(:target_final_amount)   { Money("41.5") }
+
+      it "should exchange only the money that is available on the source account" do
+        mock(source_account).withdraw(source_initial_amount)
+        mock(target_account).deposit(target_final_amount)
+        mock(calculator).compute_target_amount(source_initial_amount,rate) { target_final_amount }
+
+        exchanger.exchange(:source => limit)
+      end
+    end
   end
 
   context "with a limit specified for the target currency" do
@@ -55,6 +68,20 @@ describe CurrencyExchanger do
       mock(calculator).compute_target_amount(source_amount,rate) { target_final_amount }
 
       exchanger.exchange(:target => limit)
+    end
+
+    context "with the limit exceeding the money on the source account" do
+      let(:source_initial_amount) { Money("10") }
+      let(:target_final_amount)   { Money("41.5") }
+
+      it "should exchange only the money that is available on the source account" do
+        mock(source_account).withdraw(source_initial_amount)
+        mock(target_account).deposit(target_final_amount)
+        mock(calculator).compute_source_amount(limit,rate) { source_amount }
+        mock(calculator).compute_target_amount(source_initial_amount,rate) { target_final_amount }
+
+        exchanger.exchange(:target => limit)
+      end
     end
   end
 end
