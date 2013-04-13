9fb677223759084ef42e6fe75be9d32a99885e06
Aleksander Pohl <apohllo@o2.pl>
2013-04-13 15:40:52 +0200

    Add tests for money exchanger with specific amount
    It should be possible to specify the limit for source and target currency in
    order to exchange only that amount of money.
    
    Don't use Float for representing the amount of money. Use strings indstead
    allowing for using arbitrary representation, that performs accurate decimal
    arithmetics.

--- task-3/test/acceptance/money_exchanger.rb
+++ task-3/test/acceptance/money_exchanger.rb
@@ -8,11 +8,29 @@ currency) and exchange the many according to exchange rates.
 describe "currency exchanger" do
   context "user with two bank accounts EUR and PLN" do
     specify "exchange of all money from the EUR account" do
-      set_balance :eur => 100, :pln => 0
-      set_exchange_rate [:eur,:pln] => 4.15
-      exchange_money :eur,:pln
-      get_balance(:eur).should == 0
-      get_balance(:pln).should == 415
+      set_balance :eur => "100", :pln => "0"
+      set_exchange_rate [:eur,:pln] => "4.15"
+      exchange_money :eur, :pln
+      get_balance(:eur).should == "0"
+      get_balance(:pln).should == "415"
+    end
+
+    specify "exchange of money with specified amount of the source currency" do
+      set_balance :eur => "100", :pln => "0"
+      set_exchange_rate [:eur,:pln] => "4.15"
+      exchange_money :eur, :pln, :eur => "50"
+      get_balance(:eur).should == "50"
+      get_balance(:pln).should == "205.75"
+    end
+
+    specify "exchange of money with specified amount of the target currency" do
+      set_balance :eur => "100", :pln => "0"
+      set_exchange_rate [:eur,:pln] => "4.15"
+      exchange_money :eur, :pln, :pln => "200.03"
+      get_balance(:eur).should == "51.80"
+      #The resulting amount of target currency should be no-less than the specified
+      #value, if the exact value cannot be obtained.
+      get_balance(:pln).should == "200.03"
     end
   end
 end
